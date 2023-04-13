24a0ca71f21de96d3245e257efcf6d451a91473b
Joice Marina <joicemarinag@gmail.com>
2023-04-13 14:53:08 -0300

    Adiciona ícones de redes sociais no footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,47 @@
 import styles from './Footer.module.css';
-import Logo from '../../Logo';
+import Logo from './assets/Logo';
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className={styles.root}>
-      <div className={styles.contents}>
-        <div className={styles.logo}>
-          <Logo />
+    <footer className={styles['footer-contents']}>
+      <div className={styles['footer-column']}>
+        <div className={styles['logo']}>
+          <Logo color='#555555' />
         </div>
-        <div className={styles.footer}>
-          <div className={styles.copyright}>
+        <div className={styles['footer']}>
+          <div className={styles['footer-copyright']}>
             <p>Copyright © 2023 NekoApp Inc. Todos os direitos reservados.</p>
+          </div>
+          <div class={styles['social-icons']}>
+            <p>Siga-nos</p>
+            <ul class={styles['social-icons_list']}>
+              <li class={styles['social-icons_item']}>
+                <a href="https://www.facebook.com/joicemarina" target="_blank" rel="noopener noreferrer">
+                  <FaFacebook color='#000' />
+                </a>
+              </li>
+              <li class={styles['social-icons_item']}>
+                <a href="https://twitter.com/marigrigolato" target="_blank" rel="noopener noreferrer">
+                  <FaTwitter color='#000' />
+                </a>
+              </li>
+              <li class={styles['social-icons_item']}>
+                <a href="https://www.instagram.com/marigrigolato/" target="_blank" rel="noopener noreferrer">
+                  <FaInstagram color='#000' />
+                </a>
+              </li>
+              <li class={styles['social-icons_item']}>
+                <a href="https://www.linkedin.com/in/joice-grigolato/" target="_blank" rel="noopener noreferrer">
+                  <FaLinkedin color='#000' />
+                </a>
+              </li>
+              <li class={styles['social-icons_item']}>
+                <a href="https://github.com/marigrigolato" target="_blank" rel="noopener noreferrer">
+                  <FaGithub color='#000' />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
--- src/components/Footer/assets/Logo.jsx
+++ src/components/Footer/assets/Logo.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Logo = () => {
+const Logo = ({ color }) => {
   return (
     <svg height="4.7em" version="1.1" viewBox="0 -20 460 180" width="17.5em" xmlns="http://www.w3.org/2000/svg">
-      <g fill="#555555">
+      <g fill={color || '#555555'}>
         <ellipse cx="200" cy="220" rx="195" ry="180"/>
         <g overflow="visible" transform="translate(110 130)">
           <path transform="rotate(-32)" d="m -85 0 c 0 -5 55 -150 85 -150 c 30 0 85 145 85 150"/>
